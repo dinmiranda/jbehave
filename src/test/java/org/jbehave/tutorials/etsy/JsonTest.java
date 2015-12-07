cea97e753428ef5456e9c9bc0fa6aaf7f8fbceaf
dokala <dinmiranda@gmail.com>
2015-12-07 08:10:26 +0800

    upgrade jbehave plugins and update test for common filename edit

--- src/test/java/org/jbehave/tutorials/etsy/JsonTest.java
+++ src/test/java/org/jbehave/tutorials/etsy/JsonTest.java
@@ -18,73 +18,68 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * Created by dad on 12/3/15.
  */
 public class JsonTest {
 
-    @Test
-    public void jacksonObjectWriteTest() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        Path file = Paths.get(
-                "/media/BigDrive/Projects/workspaceM/jbehave/src/test/resources/org/jbehave/tutorials/etsy/test.json" );
-        BufferedReader br = Files.newBufferedReader(file, Charset.forName("UTF-8"));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null)
-            sb.append(line);
-        System.out.println("RAW String is: " + sb.toString());
+	String filePath = "/home/dokala/workspaceM/jbehave/src/test/resources/org/jbehave/tutorials/etsy/test.json";
 
-        JsonFactory f = new JsonFactory();
-        JsonParser jp = f.createParser(sb.toString());
-        while (jp.nextToken() == JsonToken.START_OBJECT) {
-            Object content = mapper.readValue( jp, Object.class );
-            // process
-            // after binding, stream points to closing END_OBJECT
-            System.out.println("Object is: " + content.toString());
-            System.out.println( "Content is: " + ( ( Map ) content ).get( "Content" ) );
-            List content1 = ( List ) ( ( Map ) content ).get( "Content" );
-            System.out.println( "Item is: " + content1.get( 0 ) );
-        }
-    }
+	@Test
+	public void jacksonObjectWriteTest() throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Path file = Paths.get(filePath);
+		BufferedReader br = Files.newBufferedReader(file, Charset.forName("UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null)
+			sb.append(line);
+		System.out.println("RAW String is: " + sb.toString());
 
+		JsonFactory f = new JsonFactory();
+		JsonParser jp = f.createParser(sb.toString());
+		while (jp.nextToken() == JsonToken.START_OBJECT) {
+			Object content = mapper.readValue(jp, Object.class);
+			// process
+			// after binding, stream points to closing END_OBJECT
+			System.out.println("Object is: " + content.toString());
+			System.out.println("Content is: " + ((Map) content).get("Content"));
+			List content1 = (List) ((Map) content).get("Content");
+			System.out.println("Item is: " + content1.get(0));
+		}
+	}
 
-    @Test
-    public void jacksonNodeTest() throws IOException {
-        byte[] jsonData = Files.readAllBytes( Paths.get(
-                "/media/BigDrive/Projects/workspaceM/jbehave/src/test/resources/org/jbehave/tutorials/etsy/test.json"
-                                                       ) );
-        ObjectMapper mapper = new ObjectMapper();
-        //create JsonNode
-        JsonNode rootNode = mapper.readTree( jsonData );
-        System.out.println( "Object is: " + rootNode.toString() );
-        JsonNode content = rootNode.path( "Content" );
-        System.out.println( "Content is: " + content.toString() );
-        System.out.println( "Content type is: " + content.isArray() );
-        System.out.println( "Content class is: " + content.getClass() );
-        for( int i = 0; i < content.size(); i++ ) {
-            System.out.println( "Content class item: " + content.get( i ) );
-            Content item = new Content();
-            item.setAssignedTo( content.get( i ).path( "assignedTo" ).textValue() );
-            System.out.println( "Content class item: " + content.get( i ).path( "collections" ).getClass() );
-        }
-    }
+	@Test
+	public void jacksonNodeTest() throws IOException {
+		byte[] jsonData = Files.readAllBytes(Paths.get(filePath));
+		ObjectMapper mapper = new ObjectMapper();
+		// create JsonNode
+		JsonNode rootNode = mapper.readTree(jsonData);
+		System.out.println("Object is: " + rootNode.toString());
+		JsonNode content = rootNode.path("Content");
+		System.out.println("Content is: " + content.toString());
+		System.out.println("Content type is: " + content.isArray());
+		System.out.println("Content class is: " + content.getClass());
+		for (int i = 0; i < content.size(); i++) {
+			System.out.println("Content class item: " + content.get(i));
+			Content item = new Content();
+			item.setAssignedTo(content.get(i).path("assignedTo").textValue());
+			System.out.println("Content class item: " + content.get(i).path("collections").getClass());
+		}
+	}
 
-    @Test
-    public void jacksonReadValTest() throws IOException {
-        byte[] jsonData = Files.readAllBytes( Paths.get(
-                "/media/BigDrive/Projects/workspaceM/jbehave/src/test/resources/org/jbehave/tutorials/etsy/test.json"
-                                                       ) );
-        ObjectMapper mapper = new ObjectMapper();
-        ContentWrapper content = mapper.readValue( jsonData, ContentWrapper.class );
+	@Test
+	public void jacksonReadValTest() throws IOException {
+		byte[] jsonData = Files.readAllBytes(Paths.get(filePath));
+		ObjectMapper mapper = new ObjectMapper();
+		ContentWrapper content = mapper.readValue(jsonData, ContentWrapper.class);
 
-        System.out.println( "An item: " + mapper.writeValueAsString( content ) );
-        for( Content item : content.content ) {
-            System.out.println( item.getSection() );
-            System.out.println( item.getAssignedTo() );
-            System.out.println( item.getCollections().get( "Analytics" ) );
+		System.out.println("An item: " + mapper.writeValueAsString(content));
+		for (Content item : content.content) {
+			System.out.println(item.getSection());
+			System.out.println(item.getAssignedTo());
+			System.out.println(item.getCollections().get("Analytics"));
 
-        }
-    }
+		}
+	}
 }
